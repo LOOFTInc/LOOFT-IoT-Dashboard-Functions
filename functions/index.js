446a1619732de19fd75ff306e41713a327a50dbf
Awais Amjed <awaisamjed171@gmail.com>
2023-04-07 13:47:50 +0500

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,109 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+admin.initializeApp();
+
+exports.onRealtimeUpdate = functions.firestore.document('Looft/{deviceID}/realtime/data').onUpdate(async (change, context) => {
+  const newValue = change.after.data();
+
+  let deviceName = (await admin.firestore().doc('Data/Devices').get()).data()[context.params.deviceID]['name'];
+  deviceName += '\n\n';
+
+  let email = '';
+  await admin.firestore().collection(`Looft/${context.params.deviceID}/listeners`).get().then(
+    (value) => {
+      value.docs.forEach((doc) => {
+        const comparison = doc.data()['comparison'];
+        const metric = doc.data()['metric'];
+        const threshold = Number(doc.data()['threshold']);
+
+        if (compare(comparison, newValue[metric], threshold) === true) {
+          email += `${firebaseVariableToString(metric)} is ${comparisonFromString(comparison)} ${threshold}\n`;
+        }
+      });
+    }
+  );
+
+  if (email !== '') {
+    await sendEmail('iot-dashboard@looft <noreply@firebase.com>', 'awais@looft.io', 'Dashboard Threshold', deviceName + email);
+  }
+});
+
+const compare = (string, value, threshold) => {
+  if (string === '<') {
+    return value < threshold;
+  } else if (string === '>') {
+    return value > threshold;
+  } else if (string === '<=') {
+    return value <= threshold;
+  } else if (string === '>=') {
+    return value >= threshold;
+  } else {
+    return value === threshold;
+  }
+}
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+const sendEmail = async (from, to, subject, body) => {
+  const mailOptions = {
+    from: from,
+    to: to,
+  };
+
+  // The user subscribed to the newsletter.
+  mailOptions.subject = subject;
+  mailOptions.text = body;
+  return await mailTransport.sendMail(mailOptions)
+    .then((e) => {
+      return {'result': e};
+    });
+}
+
+const firebaseVariableToString = (variable) => {
+  switch (variable) {
+    case 'Otemperature':
+      return 'Outside Temperature';
+    case 'Ihumidity':
+      return 'Inside Humidity';
+    case 'Ohumidity':
+      return 'Outside Humidity';
+    case 'Current':
+      return 'Current';
+    case 'RealPower':
+      return 'Real Power';
+    case 'coilInTemp':
+      return 'In Coil Temperature';
+    case 'coilOutTemp':
+      return 'Out Coil Temperature';
+    case 'FreeHEAP':
+      return 'Free RAM';
+    case 'maxHEAP':
+      return 'Max RAM Usage';
+    default:
+      return 'Inside Temperature';
+  }
+}
+
+const comparisonFromString = (value) => {
+  switch (value) {
+    case '>':
+      return 'Greater than' ;
+    case '<':
+      return 'Less than';
+    case '>=':
+      return 'Greater than or Equal to';
+    case '<=':
+      return 'Less than or Equal to';
+    default:
+      return 'Equal To';
+  }
+}
