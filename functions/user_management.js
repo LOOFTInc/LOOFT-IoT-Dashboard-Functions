d9609024f5fea5d017bfa8eaa85b728fb4fca5e9
Awais Amjed <awaisamjed171@gmail.com>
2023-10-05 16:23:53 +0500

    Add function for fetching all Users

---
+++ functions/user_management.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const {getAuth} = require("firebase-admin/auth");
+admin.initializeApp();
+
+exports.getAllUsers = functions.https.onCall(async (data, context) => {
+  try {
+    const role = context.auth.token.role;
+
+    if (role !== 'super_admin' && role !== 'admin') {
+      return {
+        error: 'You are not authorized to perform this action.'
+      };
+    }
+
+    let company;
+    if (role === 'super_admin') {
+      company = data.company;
+    } else {
+      company = context.auth.token.company;
+    }
+
+    const users = [];
+    await getAuth().listUsers(10000).then((value) => {
+      value.users.forEach((user) => {
+        if (user.customClaims.company === company) {
+          users.push(user);
+        }
+      });
+    });
+
+    return {
+      result: users
+    };
+  } catch (e) {
+    return {
+      error: e
+    };
+  }
+});
+
+exports.test = functions.https.onCall(async (data, context) => {
+
+});
